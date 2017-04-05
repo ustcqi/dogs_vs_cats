62aae5cc5e4063c4d33937892b7167330fc16234
齐超 <qichao@aisino.com>
2017-04-05 19:42:47 +0800

    update preprocessing.sh

--- shell/preprocessing.sh
+++ shell/preprocessing.sh
@@ -1,11 +1,13 @@
+rm -rf ../data/train/dog ../data/train/cat
 ls ../data/train/ > t.log
+mkdir ../data/train/dog ../data/train/cat
 cat t.log | while read line
 do
     result=$(echo $line | grep "dog")
     if  [[ "$result" != "" ]]
     then
         echo "$line, move ../data/train/$line to ../data/train/dog/"
-        mv ./train/$line ./dog/
+        mv ../data/train/$line ../data/train/dog/
     else
         echo "$line, move ../data/train/$line to ../data/train/cat/"
         mv ../data/train/$line ../data/train/cat/
